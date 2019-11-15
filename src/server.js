bde8d2ec535684bd10fb96c8227eec5c33aaa67d
Petherson Moreno <pethermoreno@gmail.com>
2019-11-15 15:11:58 -0300

    feat: initialized firebase config, added router and bodyparser

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
-const Koa = require("koa");
+const firebase = require('firebase/app');
+require('firebase/firestore');
+const Koa = require('koa');
+const koaRouter = require('koa-route');
+const bodyParser = require('koa-bodyparser');
+const firebaseConfig = require('../firebaseConfig.json');
+
+firebase.initializeApp(firebaseConfig);
+
 const app = new Koa();
 
-app.use(async ctx => {
-    ctx.body = "Hello World";
-});
+app.use(bodyParser());
+
+app.use(
+  koaRouter.get('/', async ctx => {
+    ctx.body = 'index test';
+  })
+);
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log("Server started http://localhost:" + port);
+  console.log(`Server started http://localhost:${port}`);
 });
