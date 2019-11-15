ef3e23b7af302774d1cfa917f00b9283ba21b42a
Petheson Moreno <pethermoreno@gmail.com>
2019-11-15 16:46:22 -0300

    Feat/changed firebase config to env (#6)
    * chore: added dotenv dependency
    
    * chore: changed firebase config to environment variables

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
 const firebase = require('firebase/app');
 require('firebase/firestore');
+require('dotenv').config();
 const Koa = require('koa');
 const koaRouter = require('koa-route');
 const bodyParser = require('koa-bodyparser');
-const firebaseConfig = require('../firebaseConfig.json');
 const peopleRoutes = require('./people/routes');
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  projectId: process.env.FIREBASE_PROJECT_ID
+});
 
 const app = new Koa();
 
 app.use(bodyParser());
 
-peopleRoutes.map(({ path, method, handler }) =>
-  app.use(koaRouter[method](path, handler))
-);
+peopleRoutes.map(({ path, method, handler }) => app.use(koaRouter[method](path, handler)));
 
 app.use(
   koaRouter.get('/', async ctx => {
@@ -25,6 +26,7 @@ app.use(
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server started http://localhost:${port}`);
 });
 
