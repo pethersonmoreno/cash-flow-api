b77997a5f6cfeef2fc99fcc8660cc90d0dd2837e
Petherson Moreno <pethermoreno@gmail.com>
2019-11-15 15:35:23 -0300

    feat: added route /people

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ const Koa = require('koa');
 const koaRouter = require('koa-route');
 const bodyParser = require('koa-bodyparser');
 const firebaseConfig = require('../firebaseConfig.json');
+const peopleRoutes = require('./people/routes');
 
 firebase.initializeApp(firebaseConfig);
 
 const app = new Koa();
 
 app.use(bodyParser());
+
+peopleRoutes.map(({ path, method, handler }) => app.use(koaRouter[method](path, handler)));
 
 app.use(
   koaRouter.get('/', async ctx => {
