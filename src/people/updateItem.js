366a295b6e17d834f9c4c7d7b03d6ebc0d28bc19
Petheson Moreno <pethermoreno@gmail.com>
2019-11-16 19:12:11 -0300

    Update updateItem.js

--- src/people/updateItem.js
+++ src/people/updateItem.js
@@ -1,18 +1,18 @@
 const documentExists = require('../infra/firestore/documentExists');
 const updateDocument = require('../infra/firestore/updateDocument');
+const schema = require ('../schemas/person');
 
 const updateItem = async (req, res) => {
   const { id } = req.params;
-  const item = { ...req.body };
   if (await documentExists('people', id)) {
-    await updateDocument('people', id, item);
-    res.json({
-      id,
-      ...item
-    });
-  } else {
     res.status(404).send('Not found');
   }
+  const { error, item } = schema.validate(req.body);
+  if(error){
+    return req.status(400).send(error.message);
+  }
+  await updateDocument('people', id, item);
+  res.json({ id, ...item });
 };
 
 module.exports = updateItem;
