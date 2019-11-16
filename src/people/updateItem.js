25c1654cdc1a450580410f10489f405aaaf85740
Petheson Moreno <pethermoreno@gmail.com>
2019-11-16 19:26:19 -0300

    Update updateItem.js

--- src/people/updateItem.js
+++ src/people/updateItem.js
@@ -2,11 +2,17 @@ const documentExists = require('../infra/firestore/documentExists');
 const updateDocument = require('../infra/firestore/updateDocument');
 const schema = require ('../schemas/person');
 
-const updateItem = async (req, res) => {
-  const { id } = req.params;
-  if (await documentExists('people', id)) {
+const blockNotFoundDocument = (collation, paramRouteName, next) => async (req, res) => {
+  const docPath = req.params[paramRouteName];
+  if (await documentExists(collation, docPath)) {
     return res.status(404).send('Not found');
   }
+  return next(req, res);
+};
+
+
+const updateItem = async (req, res) => {
+  const { id } = req.params;
   const { error, item } = schema.validate(req.body);
   if(error){
     return req.status(400).send(error.message);
@@ -15,4 +21,4 @@ const updateItem = async (req, res) => {
   res.json({ id, ...item });
 };
 
-module.exports = updateItem;
+module.exports = blockNotFoundDocument('people', 'id',updateItem);
