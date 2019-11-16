524bc267150580b65699395308696ca7e7d85ddd
Petheson Moreno <pethermoreno@gmail.com>
2019-11-16 16:15:07 -0300

    Update account.js

--- src/schemas/account.js
+++ src/schemas/account.js
@@ -1,6 +1,6 @@
 const Joi = require('@hapi/joi');
 
 module.exports=Joi.object({
-  personId: Joi.string().guid(),
+  personId: Joi.string().guid(). required(),
   description: Joi.string().min(3).max(30).required()
 });
