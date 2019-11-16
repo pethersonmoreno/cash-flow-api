b3b4e99e9fe72ba20a3773ddc7b26e709d47fa06
Petheson Moreno <pethermoreno@gmail.com>
2019-11-16 19:05:08 -0300

    Update createItem.js

--- src/people/createItem.js
+++ src/people/createItem.js
@@ -1,7 +1,12 @@
 const addDocument = require('../infra/firestore/addDocument');
+const personSchema = require('../schemas/person.js');
 
 const createItem = async (req, res) => {
-  const item = { ...req.body };
+  const { error, item } = schema.validate(req.body);
+  if(error){
+    req.status(400).send(error.message);
+    return;
+  }
   const { id } = await addDocument('people', item);
   res.json({
     id,
