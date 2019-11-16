5600c86c1448b39458c87e5139d8ffd4f6cfe94c
Petheson Moreno <pethermoreno@gmail.com>
2019-11-16 19:28:14 -0300

    Create blockNotFoundDocument.js

---
+++ src/middlewares/blockNotFoundDocument.js
@@ -0,0 +1,9 @@
+const blockNotFoundDocument = (collation, paramRouteName, next) => async (req, res) => {
+  const docPath = req.params[paramRouteName];
+  if (await documentExists(collation, docPath)) {
+    return res.status(404).send('Not found');
+  }
+  return next(req, res);
+};
+
+module.exports = blockNotFoundDocument;
