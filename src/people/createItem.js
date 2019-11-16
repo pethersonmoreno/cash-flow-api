67bb8e5a46e3a82d702b882e5b8b324cbb4104ec
Petherson Moreno <pethermoreno@gmail.com>
2019-11-15 23:48:04 -0300

    testing

--- src/people/createItem.js
+++ src/people/createItem.js
@@ -1,9 +1,10 @@
-const firebase = require('firebase/app');
+// const firebase = require('firebase/app');
 
 const createItem = async ctx => {
-  const db = firebase.firestore();
+  // const db = firebase.firestore();
   const item = { ...ctx.request.body };
-  const { id } = await db.collection('people').add(item);
+  const id = 'testing';
+  // const { id } = await db.collection('people').add(item);
   ctx.body = {
     id,
     ...item
