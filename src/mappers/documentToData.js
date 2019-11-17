fb2e61f56eb516cf04d28d4c07a7bb88ed658ea8
Petherson Moreno <pethermoreno@gmail.com>
2019-11-17 17:16:38 -0300

    feat: updated mapDocumentToData to date fields  - to convert Timestamp of Firestore to Javascript Date

--- src/mappers/documentToData.js
+++ src/mappers/documentToData.js
@@ -1,6 +1,22 @@
-const mapDocumentToData = doc => ({
-  id: doc.id,
-  ...doc.data()
-});
+const mapField = field => {
+  if (
+    field.toDate &&
+    Object.getPrototypeOf(field).constructor.name === 'Timestamp'
+  ) {
+    return field.toDate();
+  }
+  return field;
+};
+const mapDocumentToData = doc => {
+  const docData = doc.data();
+  const data = Object.keys(docData).reduce((acc, key) => {
+    acc[key] = mapField(docData[key]);
+    return acc;
+  }, {});
+  return {
+    id: doc.id,
+    ...data
+  };
+};
 
 module.exports = mapDocumentToData;
